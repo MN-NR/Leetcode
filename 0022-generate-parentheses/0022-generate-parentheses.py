17b4ebc2695f791a2266bebd30bace3001c08cd1
mn <108609236+MN-Noor@users.noreply.github.com>
2026-06-02 04:23:22 -0400

    Time: 3 ms (30.68%), Space: 19.4 MB (73.76%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,12 +4,10 @@ class Solution:
         
         def dfs(st,left_count,right_count):
             if n==left_count and n==right_count:
-                print(st)
                 result.append(st)
                 return
                 
             if left_count<n:
-                print("open", left_count, right_count, st)
                 dfs(st+"(",left_count+1,right_count)
             if left_count>right_count:
                 dfs(st+")",left_count,right_count+1)
