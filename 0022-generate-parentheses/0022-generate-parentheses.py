00a601f26f7a14c84fc58e711ba1cf792ab1ecef
mn <108609236+MN-Noor@users.noreply.github.com>
2026-06-02 04:22:51 -0400

    Time: 3 ms (30.68%), Space: 19.4 MB (35.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,25 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack=[]
-        res=[]
-        def backtrack(Nopen,Nclose):
-            if Nopen==Nclose==n:
-                res.append("".join(stack))
+        result=[]
+        
+        def dfs(st,left_count,right_count):
+            if n==left_count and n==right_count:
+                print(st)
+                result.append(st)
                 return
-            if Nopen<n:
-                stack.append('(')
-                backtrack(Nopen+1,Nclose)
-                stack.pop()
-            if Nclose<Nopen:
-                stack.append(')')
-                backtrack(Nopen,Nclose+1)
-                stack.pop()
                 
-        backtrack(0,0)
-        return res
+            if left_count<n:
+                print("open", left_count, right_count, st)
+                dfs(st+"(",left_count+1,right_count)
+            if left_count>right_count:
+                dfs(st+")",left_count,right_count+1)
+        dfs("",0,0)
+        return result
 
             
-
-
-
-      
+        
