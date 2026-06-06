1fa25d3036acbad3d8e71d007617cacf12b5a84a
mn <108609236+MN-NR@users.noreply.github.com>
2026-06-06 01:32:04 -0400

    Time: 11 ms (47.97%), Space: 19.5 MB (64.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,23 +1,17 @@
-class Solution(object):
-    def combinationSum(self, candidates, target):
-       ans=[]
-       self.getAllCombinations(ans, [], 0, candidates, target)
-       return ans
-    def getAllCombinations(self,ans,combin,index,candidates,target):
-        if index==len(candidates) or target<0:
-            return
-        if target==0:
-            ans.append(list(combin))
-            return
-
-        combin.append(candidates[index])
-        # # single
-        # self.getAllCombinations(ans,combin,index+1,candidates,target-candidates[index])
-        # multiple
-        self.getAllCombinations(ans,combin,index,candidates,target-candidates[index])
-        combin.pop()
-        # exclusion
-        self.getAllCombinations(ans,combin,index+1,candidates,target)
-
-
-    
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        def dfs(i,arrsum,arr):
+            if arrsum==target:
+                result.append(arr)
+            elif arrsum>target or i+1>len(candidates):
+                return
+            else:
+                dfs(i,arrsum+candidates[i],arr+[candidates[i]])
+                for j in range(i,len(candidates)-1):
+                    dfs(j+1,arrsum+candidates[j+1],arr+[candidates[j+1]])
+               
+        result=[]
+        for i in range(len(candidates)):
+            dfs(i,candidates[i],[candidates[i]])
+        return result
